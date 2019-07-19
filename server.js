639e5f779c6c1ce06c9e2fe9b1f2a76396334636
BestVersion7 <earthsmoonandspoon@yahoo.com>
2019-07-18 21:27:43 -0400

    sitemap package

--- server.js
+++ server.js
@@ -49,9 +49,7 @@ const sitemap = sm.createSitemap({
     cacheTime: 600000, // 600 sec - cache purge period
     urls: [
         { url: "/page-1/", changefreq: "daily", priority: 0.3 },
-        { url: "/page-2/", changefreq: "monthly", priority: 0.7 },
-        { url: "/page-3/" }, // changefreq: 'weekly',  priority: 0.5
-        { url: "/page-4/", img: "http://urlTest.com" }
+        { url: "/page-2/", changefreq: "monthly", priority: 0.7 }
     ]
 });
 
