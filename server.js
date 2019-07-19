2cc972c0dd077fd35af8ce418377a4209274b156
BestVersion7 <earthsmoonandspoon@yahoo.com>
2019-07-18 21:11:16 -0400

    add sitemap

--- server.js
+++ server.js
@@ -1,53 +1,77 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const morgan = require('morgan');
-require('dotenv').config()
-const cloudinary = require('cloudinary').v2
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+const cookieParser = require("cookie-parser");
 
 const app = express();
-const movieRouter = require('./api/routes/movie')
-const userRouter = require('./api/routes/user')
-const galleryRouter = require('./api/routes/gallery')
-const drinkRouter = require('./api/routes/drink')
+const movieRouter = require("./api/routes/movie");
+const userRouter = require("./api/routes/user");
+const galleryRouter = require("./api/routes/gallery");
+const drinkRouter = require("./api/routes/drink");
+const sm = require("sitemap");
 
 //cross origin sharing (different domain)
-app.use(cors())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(morgan("dev"));
 
 //Bodyparser middleware
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-//connect database 
-const db = process.env.MONGO_URI
-mongoose.connect(db, {
-    useNewUrlParser: true
-})
-.then(console.log('mongoose running'))
-.catch(() => console.log('mongoose not running'))
- 
+//connect database
+const db = process.env.MONGO_URI;
+mongoose
+    .connect(db, {
+        useNewUrlParser: true
+    })
+    .then(console.log("mongoose running"))
+    .catch(() => console.log("mongoose not running"));
+
 //connect cloudinary
 cloudinary.config({
-    CLOUDINARY_URL:process.env.CLOUDINARY_URL
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL
 });
 
 //Express routes
-app.use('/api/movie', movieRouter)
-app.use('/api/user', userRouter)
-app.use('/api/drinks', drinkRouter)
+app.use("/api/movie", movieRouter);
+app.use("/api/user", userRouter);
+app.use("/api/drinks", drinkRouter);
 
 //Gallery unused
-app.use('/api/gallery', galleryRouter)
+app.use("/api/gallery", galleryRouter);
+
+const sitemap = sm.createSitemap({
+    hostname: "http://goldenwine.herokuapp.com",
+    cacheTime: 600000, // 600 sec - cache purge period
+    urls: [
+        { url: "/page-1/", changefreq: "daily", priority: 0.3 },
+        { url: "/page-2/", changefreq: "monthly", priority: 0.7 },
+        { url: "/page-3/" }, // changefreq: 'weekly',  priority: 0.5
+        { url: "/page-4/", img: "http://urlTest.com" }
+    ]
+});
+
+// sitemap
+app.get("/sitemap.xml", (req, res) => {
+    sitemap.toXML(function(err, xml) {
+        if (err) {
+            return res.status(500).end();
+        }
+        res.header("Content-Type", "application/xml");
+        res.send(xml);
+    });
+});
 
 //serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === "production") {
     //set static folder
-    app.use(express.static('client/build'));
-    app.get('*',(req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
@@ -56,6 +80,5 @@ const port = process.env.PORT || 4000;
 
 //Start server
 app.listen(port, () => {
-    console.log(`Server is on ${port}`)
-})
-
+    console.log(`Server is on ${port}`);
+});
